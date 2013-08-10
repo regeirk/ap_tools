21e58fb6e7b1aaeb7db5b85c6d893a50412a8dfe
paloczy <paloczy@gmail.com>
2013-08-10 11:30:06 -0300

    Organizing.

---
+++ ap_tools/utils/utils.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Description: Loose scripts and functions of personal use.
+#
+# Author:      André Palóczy Filho
+# E-mail:      paloczy@gmail.com
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def helloworld():
+  np.disp('Hello there, Mr. World.')
+
+if __name__=='__main__':
+  helloworld()
